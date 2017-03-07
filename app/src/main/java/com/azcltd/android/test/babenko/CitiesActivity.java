19361d7f308c0383a5e8887c3e017c9e83226583
Roman Babenko <rbab@yandex.ru>
2017-03-07 23:48:01 +0700

    Inited project

---
+++ app/src/main/java/com/azcltd/android/test/babenko/CitiesActivity.java
@@ -0,0 +1,12 @@
+package com.azcltd.android.test.babenko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
