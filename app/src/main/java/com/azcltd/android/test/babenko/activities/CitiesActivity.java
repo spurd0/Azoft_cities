c98c77f520c91f644403325ef2cb14472a6a9d1b
Roman Babenko <rbab@yandex.ru>
2017-03-08 21:01:30 +0700

    Added dialog if error occurs

--- app/src/main/java/com/azcltd/android/test/babenko/activities/CitiesActivity.java
+++ app/src/main/java/com/azcltd/android/test/babenko/activities/CitiesActivity.java
@@ -1,10 +1,14 @@
 package com.azcltd.android.test.babenko.activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.azcltd.android.test.babenko.BuildConfig;
 import com.azcltd.android.test.babenko.R;
 import com.azcltd.android.test.babenko.data.Cities;
@@ -12,7 +16,8 @@ import com.azcltd.android.test.babenko.managers.CitiesManager;
 
 public class CitiesActivity extends AppCompatActivity {
     private static final String TAG = "CitiesActivity";
-    private ListView mCitiesLV;
+    private ListView mCitiesLv;
+    private TextView mConnectionErrorTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +28,49 @@ public class CitiesActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        mCitiesLV = (ListView) findViewById(R.id.citiesLv);
+        mCitiesLv = (ListView) findViewById(R.id.citiesLv);
+        mConnectionErrorTv = (TextView) findViewById(R.id.connectionErrorTv);
     }
 
     private void requestCities() {
         CitiesManager.getInstance().requestCities(new CitiesManager.CitiesRequestCallback() {
             @Override
             public void gotCities(Cities cities) {
+                if (cities == null) {
+                    showServerErrorDialog(getString(R.string.error_dialog_null_response));
+                    return;
+                }
                 if (BuildConfig.DEBUG) Log.d(TAG, cities.toString());
+                /// TODO: 08/03/17 threadpool for loading images
             }
 
             @Override
             public void responseError(Throwable t) {
                 if (BuildConfig.DEBUG) Log.e(TAG, t.toString());
+                showServerErrorDialog(t.getMessage());
             }
         });
+    }
+
+    private void showServerErrorDialog(String content) {
+        new MaterialDialog.Builder(CitiesActivity.this)
+                .title(R.string.error_dialog_title)
+                .content(content)
+                .positiveText(R.string.error_dialog_try_again)
+                .negativeText(android.R.string.cancel)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        requestCities();
+                    }
+                })
+                .onNegative(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        Log.d(TAG, "onNegative");
+                    }
+                })
+                .cancelable(false)
+                .show();
     }
 }
