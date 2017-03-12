3093b5a888a45c40e9261ec87524647cd057ede6
Roman Babenko <rbab@yandex.ru>
2017-03-12 17:45:52 +0700

    Added loading image for details activity

--- app/src/main/java/com/azcltd/android/test/babenko/activities/CityDetailsActivity.java
+++ app/src/main/java/com/azcltd/android/test/babenko/activities/CityDetailsActivity.java
@@ -2,9 +2,19 @@ package com.azcltd.android.test.babenko.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.azcltd.android.test.babenko.R;
+import com.azcltd.android.test.babenko.constants.ApplicationConstants;
+import com.azcltd.android.test.babenko.utils.UtilsHelper;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 /**
  * Created by Roman Babenko (babe-roman@yandex.ru) on 12/03/17.
@@ -15,13 +25,57 @@ public class CityDetailsActivity extends AppCompatActivity {
     public static final String DESCRIPTION_KEY = "com.azcltd.android.test.babenko.activities.CityDetailsActivity.DESCRIPTION_KEY";
     public static final String IMAGE_PATH_KEY = "com.azcltd.android.test.babenko.activities.CityDetailsActivity.IMAGE_PATH_KEY";
 
+    private ImageView mCityImage;
+    private TextView mCityDescription;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.city_details_layout);
+        initViews();
+    }
 
+    private void initViews() {
+        mCityImage = (ImageView) findViewById(R.id.city_details_image);
+        mCityDescription = (TextView) findViewById(R.id.city_details_description);
         Intent intent = getIntent();
         String description = intent.getExtras().getString(DESCRIPTION_KEY);
-        String imagePath = intent.getExtras().getString(IMAGE_PATH_KEY);
+        final String imagePath = intent.getExtras().getString(IMAGE_PATH_KEY);
         Log.d(TAG, "imagepath " + imagePath);
+        mCityDescription.setText(description);
+        mCityImage.post(new Runnable() {
+            @Override
+            public void run() {
+                int size = mCityImage.getWidth();
+
+                if (UtilsHelper.checkStoragePermissions(CityDetailsActivity.this)) {
+                    File imageFile = new File(
+                            Environment.getExternalStorageDirectory().getPath()
+                                    + "/" + ApplicationConstants.APPLICATION_FOLDER + "/" + imagePath);
+                    if (!imageFile.exists()) {
+                        loadImageFromUrl(imagePath, size);
+                        return;
+                    }
+                    Picasso.with(CityDetailsActivity.this).load(imageFile)
+                            .resize(size, size)
+                            .centerInside()
+                            .error(R.drawable.question_mark)
+                            .into(mCityImage);
+                } else {
+                    loadImageFromUrl(imagePath, size);
+                }
+            }
+        });
+    }
+
+    private void loadImageFromUrl(String imageName, int size) {
+        String imageUrl = CityDetailsActivity.this.getResources().getString(R.string.azcltd_api_url)
+                + "/" + imageName;
+
+        Picasso.with(CityDetailsActivity.this).load(imageUrl)
+                .resize(size, size)
+                .centerInside()
+                .error(R.drawable.question_mark)
+                .into(mCityImage);
     }
 }
