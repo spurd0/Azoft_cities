f294a4fe3cf7d0eca1f794eb3ef2b5d9265b1860
Roman Babenko <rbab@yandex.ru>
2017-03-12 16:55:59 +0700

    Invalidating lv only if all images downloaded.

--- app/src/main/java/com/azcltd/android/test/babenko/activities/CitiesActivity.java
+++ app/src/main/java/com/azcltd/android/test/babenko/activities/CitiesActivity.java
@@ -23,6 +23,7 @@ import com.azcltd.android.test.babenko.managers.ImageManager;
 import com.azcltd.android.test.babenko.utils.UtilsHelper;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CitiesActivity extends AppCompatActivity {
     private static final String TAG = "CitiesActivity";
@@ -186,29 +187,40 @@ public class CitiesActivity extends AppCompatActivity {
     }
 
     private void downloadImages() {
+        final AtomicInteger counter = new AtomicInteger(0);
         if (UtilsHelper.checkStoragePermissions(this))
             if (mCityList != null)
                 for (final City city : mCityList)
-                    if (!city.image_url.isEmpty())
+                    if (!city.image_url.isEmpty()) {
+                        counter.incrementAndGet();
                         ImageManager.getInstance().downloadImage(city.image_url, new ImageManager.ImageCallback() {
                             @Override
                             public void imageDownloaded() {
-                                CitiesActivity.this.runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-//                                        if (mCitiesAdapter != null)
-//                                            mCitiesAdapter.notifyDataSetInvalidated();
-                                        mCitiesLv.invalidateViews();
-                                    }
-                                });
+                                if (BuildConfig.DEBUG)
+                                    Log.d(TAG, "Image loaded " + city.image_url);
+                                if (counter.decrementAndGet() == 0)
+                                    CitiesActivity.this.runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            mCitiesLv.invalidateViews();
+                                        }
+                                    });
                             }
 
                             @Override
                             public void failedToLoadImage() {
                                 if (BuildConfig.DEBUG)
                                     Log.e(TAG, "Failed to load image " + city.image_url);
+                                if (counter.decrementAndGet() == 0)
+                                    CitiesActivity.this.runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            mCitiesLv.invalidateViews();
+                                        }
+                                    });
                             }
                         });
+                    }
     }
 
     private void showServerErrorDialog(String content) {
